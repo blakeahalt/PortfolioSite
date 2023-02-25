a8189a1fd1b80a160546812080c8379ee0e9d9cc
Blake Ahalt <blakeahalt@Work.local>
2023-02-24 23:43:22 -0800

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'bb62187cfda4424f19040abd53a996cd625ff0e3',
+        'reference' => '183c7452fefd8fb70046e0f09e12f13f550d3dc9',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'bb62187cfda4424f19040abd53a996cd625ff0e3',
+            'reference' => '183c7452fefd8fb70046e0f09e12f13f550d3dc9',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
